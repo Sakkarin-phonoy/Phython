44266ae897a87eb3b1b5890e5d0bbe08aaade7ba
sakkarin <60848082+Sakkarin-phonoy@users.noreply.github.com>
2021-01-07 18:54:42 +0700

    Add files via upload

---
+++ คำนวณเกรดจากไฟล์/StudentGrade.py
@@ -0,0 +1,23 @@
+try:
+    fr=open("Score.txt","r",encoding="utf-8")
+    fw=open("Grade.txt","w",encoding="utf-8")
+    grade=None
+    for line in fr.readlines():
+        score=line[-4:].strip() # คะแนนนักเรียน
+        studentid=line[:-4].strip() # รหัสนักเรียน
+        # print("รหัส = ",studentid," คะแนน = ",score)
+        score=int(score)
+        if score>=80:
+            grade="A"
+        elif score>=70 and score<80:
+            grade="B"
+        elif score>=50 and score<=69:
+            grade="C"
+        else:
+            grade="F"
+        fw.writelines(studentid+"\t"+str(score)+"\t"+grade+"\n")
+    fw.close()
+    fr.close()
+    
+except Exception as e:
+    print(e)
